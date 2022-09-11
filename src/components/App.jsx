ec525cf42ba0840d03455d9e472694a6cdc5b7ba
Taras Shapovalov <tarasshapovalov85@gmail.com>
2022-09-11 12:39:09 +0300

    delete persist and local storage, add start file

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import style from 'components/App.module.css';
+import ContactForm from 'components/ContactForm/ContactForm';
+import Filter from 'components/Filter/Filter';
+import ContactList from 'components/ContactList/ContactList';
+
+export default function App() {
+  return (
+    <div>
+      <h2 className={style.title}>Phonebook</h2>
+      <ContactForm />
+      <h3 className={style.title}>Contacts</h3>
+      <Filter />
+      <ContactList />
+    </div>
+  );
+}
